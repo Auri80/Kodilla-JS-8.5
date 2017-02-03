a82efa73d5b38bfbfa7ab884a71099d2c8db6ce0
Agii Uri <Agii Uri>
2017-02-03 18:06:55 +0100

    if else

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
-
-
 var a = 4;
 var b = 2;
-
 var value = (a*a) + (2 * a * b) - (b*b);
 console.log(value);
 
 if (value <0){
 	console.log('wynik ujemny')
 	}
-if (value >0){
+else if (value >0){
 	console.log('wynik dodatni')
 	}
-if ( value == 0 ){
+else {
 	console.log('równe zero')
 	}
 
